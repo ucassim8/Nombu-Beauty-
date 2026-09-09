00564f9949868f5d2de2595b7e34719bfc66f90a
ucassim8 <ucassim8@users.noreply.github.com>
2026-09-09 07:08:07 +0000

    deploy: 6981ea2557467bee1918f8a6ff93f1eec5de7ac7

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "afe55245c3e03f71260255a9b7274507",
 "version.json": "2d3a76068229c9a1afd2162ba6f933a7",
-"main.dart.js": "8c31ffbc7236363758f4d6600782157b",
-"flutter_bootstrap.js": "5b3024245163de20a47b45c26343dfb9",
+"main.dart.js": "e19b81439fa71fbf7c030d25fc91d85e",
+"flutter_bootstrap.js": "01db6d30139b86800c5090fbd24ed20b",
 "canvaskit/skwasm.js": "9fa2ffe90a40d062dd2343c7b84caf01",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
 "canvaskit/chromium/canvaskit.js": "87325e67bf77a9b483250e1fb1b54677",
